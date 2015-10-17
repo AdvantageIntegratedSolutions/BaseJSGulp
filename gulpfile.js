cb49fde5115f739a7974e95d661000b861bf19a6
Kit Hensel <kith@Kits-Air.Home>
2015-10-17 00:21:15 -0600

    Auto commited by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -115,33 +115,9 @@ gulp.task('quickbase-push', function() {
       callback(xml);
     }
   }
-  
+
   req.setRequestHeader("Content-Type", "text/xml");
   req.send(data);
-
-  this.xmlPost = function(dbid, tableName, action, data, callback, handler){
-    var url = 
-    var _this = this;
-
-    data.ticket = this.ticket;
-    data = this.buildPostData(tableName, data);
-
-    var req = new XMLHttpRequest();
-    req.open("POST", url, true);
-    req.onreadystatechange = function() {
-      if(req.readyState == 4 && req.status == 200) {
-        var xml = XML.parse(req.responseText);
-        xml = handler(xml);
-
-        if(!_this.ticket && action == "API_Authenticate"){
-          _this.ticket = xml;
-        };
-
-        callback(xml);
-      }
-    }
-    req.setRequestHeader("Content-Type", "text/xml");
-    req.send(data);
 });
 
 //manually trigger deployment
