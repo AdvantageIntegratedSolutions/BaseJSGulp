fb73f862ad9c04ff71174cba90a784f4872b7d8d
Kit Hensel <kithensel@gmail.com>
2015-10-17 00:55:10 -0600

    Auto commited by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ var app = require('./app.json'),
     babel = require('gulp-babel'),
     sourcemaps = require('gulp-sourcemaps'),
     git = require('gulp-git'),
-    XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+    XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest,
+    XML = require('pixl-xml');
 
 //tasks not related to code
 var adminTasks = [
@@ -100,22 +101,25 @@ gulp.task('quickbase-push', function() {
 
   req.onreadystatechange = function() {
     if(req.readyState == 4 && req.status == 200) {
-      console.log(req.responseText);
+      var xml = XML.parse(req.responseText);
+      console.log(xml);
     };
   };
 
   req.setRequestHeader("Content-Type", "text/xml");
 
+  var ticket = "8_bkb65j6xd_by5veh_bkh_a_d86rihfd2pbmvqca5pa58d5pb2ivbjhgpnpcp4ug59dticsbhdueyhaw";
+  var pageBody = "hello";
+  var filename = "filename";
+
   var data = [];
   data.push("<qdbapi>");
   data.push.apply(data, ["<apptoken>", app.token, "</apptoken>"]);
-  data.push.apply(data, ["<ticket>", "8_bkb65j6xd_by5veh_bkh_a_d86rihfd2pbmvqca5pa58d5pb2ivbjhgpnpcp4ug59dticsbhdueyhaw", "</ticket>"]);
-  data.push.apply(data, ["<pagebody>", "hello world2", "</pagebody>"]);
+  data.push.apply(data, ["<ticket>", ticket, "</ticket>"]);
+  data.push.apply(data, ["<pagebody>", pageBody, "</pagebody>"]);
   data.push.apply(data, ["<pagetype>", "1", "</pagetype>"]);
-  data.push.apply(data, ["<pagename>", "testname", "</pagename>"]);
+  data.push.apply(data, ["<pagename>", filename, "</pagename>"]);
   data.push("</qdbapi>");
-
-  console.log(data.join(""))
 
   req.send(data.join(""));
 });
