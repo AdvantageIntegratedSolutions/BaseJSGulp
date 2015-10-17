6004aeca273051f0187f3778d1a67c6c739a05b1
Kit Hensel <kithensel@gmail.com>
2015-10-17 01:37:40 -0600

    Auto commited by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('move-pages', function() {
     .pipe(rename(function (path) {
       path.basename = app.name + "-" + path.basename;
     }))
-    .pipe(insert.prepend('<!-- '+app.origin+' -->'))
+    .pipe(insert.prepend('<!-- '+app.origin+' -->\n\n'))
     .pipe(gulp.dest('quickbase/'));
 });
 
@@ -59,7 +59,7 @@ gulp.task('build-js', function() {
     .pipe(babel())
     .pipe(uglify())
     .pipe(concat(app.name + '-'+app.jsBundlePrefix+'.js'))
-    .pipe(insert.prepend('//'+app.origin))
+    .pipe(insert.prepend('//'+app.origin+'\n\n'))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('quickbase/'));
 });
