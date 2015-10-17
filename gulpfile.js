38ada8bc7714d99b117f7d8421e1d2280a4f5bb5
Kit Hensel <kithensel@gmail.com>
2015-10-17 00:29:39 -0600

    Auto commited by gulp

--- gulpfile.js
+++ gulpfile.js
@@ -101,7 +101,8 @@ gulp.task('quickbase-push', function() {
     action: "API_AddReplaceDBPage"
   };
 
-  console.log(url)
+  console.log(url);
+  console.log(data);
 
 
   // var req = new XMLHttpRequest();
