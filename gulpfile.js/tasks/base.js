aab3450de4ee3a0907bead00723b5c1f24d1a35b
Kit Hensel <kithensel@gmail.com>
2015-12-14 16:00:04 -0700

    QBSync Auto Commit of base.js

--- gulpfile.js/tasks/base.js
+++ gulpfile.js/tasks/base.js
@@ -43,9 +43,7 @@ gulp.task('quickbase-upload', function() {
       data.push("</qdbapi>");
 
       sendQBRequest("API_AddReplaceDBPage", data.join(""));
-      return stream
     }))
-    .pipe(gulp.dest('dist'));
 });
 
 function handleXMLChars(string){
