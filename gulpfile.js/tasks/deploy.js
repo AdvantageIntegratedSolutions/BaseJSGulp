a69678d553644491caf0ee1889a2fbd184ecf0ab
Kit Hensel <kithensel@gmail.com>
2015-12-14 14:44:08 -0700

    QBSync Auto Commit of deploy.js

--- gulpfile.js/tasks/deploy.js
+++ gulpfile.js/tasks/deploy.js
@@ -20,7 +20,7 @@ gulp.task('deploy', function() {
 gulp.task('init', function(){
   git.init(function (err) {
     if (err) throw err;
-    gulp.start('addRemote');
+    gulp.start('addRemote', 'quickbase-auth');
   });
 });
 
