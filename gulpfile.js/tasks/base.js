d7561541aef976a00087ce09c0b1ac4516d12bf7
Kit Hensel <kithensel@gmail.com>
2015-12-14 15:29:27 -0700

    QBSync Auto Commit of base.js

--- gulpfile.js/tasks/base.js
+++ gulpfile.js/tasks/base.js
@@ -25,7 +25,7 @@ gulp.task('quickbase-upload', function() {
   var pageBody = "hello";
   var filename = "filename";
 
-  gulp.src('../../qb-pages/*')
+  gulp.src('../../qb-pages/*.js')
     .pipe(foreach(function(stream, file){
       console.log(file, stream)
       return stream
