e201561a1d1aaccf0fb55cef71774d9a2cfab4a4
Kit Hensel <kithensel@gmail.com>
2015-12-14 15:13:13 -0700

    QBSync Auto Commit of base.js

--- gulpfile.js/tasks/base.js
+++ gulpfile.js/tasks/base.js
@@ -25,19 +25,20 @@ gulp.task('quickbase-upload', function() {
   var filename = "filename";
 
   gulp.src('../../qb-pages').pipe(function(){
+    console.log("here")
+    // var data = [];
+    // data.push("<qdbapi>");
+    // data.push.apply(data, ["<apptoken>", app.token, "</apptoken>"]);
+    // data.push.apply(data, ["<ticket>", ticket, "</ticket>"]);
+    // data.push.apply(data, ["<pagebody>", pageBody, "</pagebody>"]);
+    // data.push.apply(data, ["<pagetype>", "1", "</pagetype>"]);
+    // data.push.apply(data, ["<pagename>", filename, "</pagename>"]);
+    // data.push("</qdbapi>");
 
+    // sendQBRequest("API_AddReplaceDBPage", data.join(""));    
   });
 
-  var data = [];
-  data.push("<qdbapi>");
-  data.push.apply(data, ["<apptoken>", app.token, "</apptoken>"]);
-  data.push.apply(data, ["<ticket>", ticket, "</ticket>"]);
-  data.push.apply(data, ["<pagebody>", pageBody, "</pagebody>"]);
-  data.push.apply(data, ["<pagetype>", "1", "</pagetype>"]);
-  data.push.apply(data, ["<pagename>", filename, "</pagename>"]);
-  data.push("</qdbapi>");
 
-  sendQBRequest("API_AddReplaceDBPage", data.join(""));
 });
 
 function sendQBRequest(action, data, mainAPICall){
