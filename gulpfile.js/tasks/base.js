ee8cc46ab2e2833ecc00dc594db21457a019468f
Kit Hensel <kithensel@gmail.com>
2015-12-14 14:37:12 -0700

    QBSync Auto Commit of base.js

--- gulpfile.js/tasks/base.js
+++ gulpfile.js/tasks/base.js
@@ -30,13 +30,10 @@ function authenticate(){
     var data = [];
     data.push("<qdbapi>");
     data.push.apply(data, ["<apptoken>", app.token, "</apptoken>"]);
-    data.push.apply(data, ["<ticket>", ticket, "</ticket>"]);
-    data.push.apply(data, ["<pagebody>", pageBody, "</pagebody>"]);
-    data.push.apply(data, ["<pagetype>", "1", "</pagetype>"]);
-    data.push.apply(data, ["<pagename>", filename, "</pagename>"]);
+    data.push.apply(data, ["<username>", ticket, "</username>"]);
+    data.push.apply(data, ["<password>", pageBody, "</password>"]);
+    data.push.apply(data, ["<hours>", "1", "</hours>"]);
     data.push("</qdbapi>");
-
-    
   };
 };
 
