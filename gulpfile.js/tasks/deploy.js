1132a1f1c2b2aa05ee4af212da2a69a105ddae21
Kit Hensel <kithensel@gmail.com>
2015-12-18 13:46:41 -0800

    QBSync Auto Commit of deploy.js

--- gulpfile.js/tasks/deploy.js
+++ gulpfile.js/tasks/deploy.js
@@ -31,7 +31,7 @@ gulp.task('rename-app', function() {
   return gulp.src(".")
     .pipe(rename(function (path) {
       console.log(path.basename)
-      console.log(path.dirname)
+      path.dirname = "Hello/"
       //path.basename = app.name;
     }))
     .pipe(gulp.dest('Hello/'));
