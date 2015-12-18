4ec40d83a5f493d7b1769a88ed8eaea9c7107f31
Kit Hensel <kithensel@gmail.com>
2015-12-17 17:22:00 -0800

    QBSync Auto Commit of javascript.js

--- gulpfile.js/tasks/javascript.js
+++ gulpfile.js/tasks/javascript.js
@@ -1,12 +1,12 @@
 var app 	     = require('../../app')
-var gulp 			 = require(NODE_PATH + 'BaseJSGulpModule/node_modules/gulp')
-var concat 		 = require(NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-concat')
-var uglify  	 = require(NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-uglify')
-var babel 		 = require(NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-babel')
-var react 		 = require(NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-react')
-var sourcemaps = require(NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-sourcemaps')
-var rename 		 = require(NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-rename')
-var insert 		 = require(NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-insert')
+var gulp 			 = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp')
+var concat 		 = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-concat')
+var uglify  	 = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-uglify')
+var babel 		 = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-babel')
+var react 		 = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-react')
+var sourcemaps = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-sourcemaps')
+var rename 		 = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-rename')
+var insert 		 = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp-insert')
 
 //compile JS
 gulp.task('js', function() {
