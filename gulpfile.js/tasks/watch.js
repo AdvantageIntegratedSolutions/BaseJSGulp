d2d5233a383acd73596fb419482825238ff8ceae
Kit Hensel <kithensel@gmail.com>
2015-12-17 17:21:51 -0800

    QBSync Auto Commit of watch.js

--- gulpfile.js/tasks/watch.js
+++ gulpfile.js/tasks/watch.js
@@ -1,4 +1,4 @@
-var gulp 			 = require(NODE_PATH + 'BaseJSGulpModule/node_modules/gulp')
+var gulp 			 = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp')
 
 //configure tasks to run on all file changes
 gulp.task('watch', function() {
