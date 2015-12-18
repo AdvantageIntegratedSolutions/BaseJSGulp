a2fd6f58a9095a8cd16a04b0d2e7f17660016c12
Kit Hensel <kithensel@gmail.com>
2015-12-17 17:09:56 -0800

    QBSync Auto Commit of javascript.js

--- gulpfile.js/tasks/javascript.js
+++ gulpfile.js/tasks/javascript.js
@@ -1,12 +1,12 @@
 var app 	     = require('../../app')
-var gulp 			 = require('BaseJSGulpModule/gulp')
-var concat 		 = require('BaseJSGulpModule/gulp-concat')
-var uglify  	 = require('BaseJSGulpModule/gulp-uglify')
-var babel 		 = require('BaseJSGulpModule/gulp-babel')
-var react 		 = require('BaseJSGulpModule/gulp-react')
-var sourcemaps = require('BaseJSGulpModule/gulp-sourcemaps')
-var rename 		 = require('BaseJSGulpModule/gulp-rename')
-var insert 		 = require('BaseJSGulpModule/gulp-insert')
+var gulp 			 = require('BaseJSGulpModule/node_modules/gulp')
+var concat 		 = require('BaseJSGulpModule/node_modules/gulp-concat')
+var uglify  	 = require('BaseJSGulpModule/node_modules/gulp-uglify')
+var babel 		 = require('BaseJSGulpModule/node_modules/gulp-babel')
+var react 		 = require('BaseJSGulpModule/node_modules/gulp-react')
+var sourcemaps = require('BaseJSGulpModule/node_modules/gulp-sourcemaps')
+var rename 		 = require('BaseJSGulpModule/node_modules/gulp-rename')
+var insert 		 = require('BaseJSGulpModule/node_modules/gulp-insert')
 
 //compile JS
 gulp.task('js', function() {
