c1bb3b88ce53d9c49769144948aaef2f15b476d5
Kit Hensel <kithensel@gmail.com>
2015-12-18 13:55:14 -0800

    QBSync Auto Commit of deploy.js

--- gulpfile.js/tasks/deploy.js
+++ gulpfile.js/tasks/deploy.js
@@ -28,7 +28,7 @@ gulp.task('init', function(){
 //rename app
 gulp.task('rename-app', function() {
 
-  return gulp.src("./*").dest("../"+app.name+"/."));
+  return gulp.src("./*").dest("../"+app.name+"/.");
 });
 
 //add remote origin
