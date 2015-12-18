848997319175712962084ffaf9f87aa1d04f227d
Kit Hensel <kithensel@gmail.com>
2015-12-17 17:23:54 -0800

    QBSync Auto Commit of default.js

--- gulpfile.js/tasks/default.js
+++ gulpfile.js/tasks/default.js
@@ -1,3 +1,3 @@
-var gulp 			 = require($NODE_PATH + 'BaseJSGulpModule/node_modules/gulp')
+var gulp 			 = require(process.env.NODE_PATH + 'BaseJSGulpModule/node_modules/gulp')
 
 gulp.task('default', ['deploy'])
