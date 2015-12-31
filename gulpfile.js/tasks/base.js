8690f7e7a962fcc400d79f4ad05bfaf099d2ab59
Kit Hensel <kithensel@gmail.com>
2015-12-31 10:58:35 -0800

    QBSync Auto Commit of base.js

--- gulpfile.js/tasks/base.js
+++ gulpfile.js/tasks/base.js
@@ -5,7 +5,7 @@ var XML            = require('pixl-xml')
 var foreach        = require('gulp-foreach')
 var path           = require('path')
 
-gulp.task('quickbase-push', function() {
+gulp.task('quickbase-push', ['html', 'js', 'css'], function() {
   var data = [];
   data.push("<qdbapi>");
   data.push.apply(data, ["<apptoken>", app.token, "</apptoken>"]);
